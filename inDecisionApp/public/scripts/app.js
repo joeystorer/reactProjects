40f825df3afd1e908daa6cbff62b139f4bb37ce5
Joey Storer <joey.storer@ascenaretail.com>
2019-01-28 14:25:22 -0500

    updating files

--- inDecisionApp/public/scripts/app.js
+++ inDecisionApp/public/scripts/app.js
@@ -1,12 +1,14 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!</p>;
 var template = React.createElement(
-  "h1",
-  { id: "someid" },
-  "Something new"
+  'h1',
+  null,
+  'Indecision App'
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
